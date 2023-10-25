608cdf29e262566e8d50f42ec9e7c8ea72d62fa9
trepvox <smylesmoriarty@gmail.com>
2023-10-25 10:51:58 -0400

    Pushing finished project for week 8 of CS50-Gaming

---
+++ Assets/Resources/Scripts/Diamond.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Diamond : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+		// despawn diamond if it goes past the left edge of the screen
+		if (transform.position.x < -25) {
+			Destroy(gameObject);
+		}
+		else {
+
+			// ensure diamond moves at the same rate as the skyscrapers do
+			transform.Translate(-SkyscraperSpawner.speed * Time.deltaTime, 0, 0, Space.World);
+		}
+
+		// infinitely rotate this diamond about the Y axis in world space
+		transform.Rotate(0, 5f, 0, Space.World);
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		// trigger diamond pickup function if a helicopter collides with this
+		other.transform.parent.GetComponent<HeliController>().PickupDiamond();
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Resources/Scripts/DiamondSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiamondSpawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start () {
+		
+		// infinite diamond spawning function, asynchronous
+		StartCoroutine(SpawnDiamonds());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator SpawnDiamonds() {
+		while (true) {
+
+			// number of diamonds we could spawn vertically
+			int diamondsThisRow = Random.Range(1, 4);
+
+			// instantiate all diamonds in this row separated by some random amount of space
+			for (int i = 0; i < diamondsThisRow; i++) {
+				Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+			}
+
+			// pause blank to blank seconds until the next diamond spawns
+			yield return new WaitForSeconds(Random.Range(5, 15));
+		}
+	}
+}
